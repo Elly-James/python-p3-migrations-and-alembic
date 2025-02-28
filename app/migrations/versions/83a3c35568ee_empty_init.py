2f5560f6afb2c713f3c65c798c0de857dd382fc3
Elly <ellyjames1999@gmail.com>
2025-02-28 15:39:15 +0300

    added

---
+++ app/migrations/versions/83a3c35568ee_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 83a3c35568ee
+Revises: 
+Create Date: 2025-02-28 15:28:51.892953
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '83a3c35568ee'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
